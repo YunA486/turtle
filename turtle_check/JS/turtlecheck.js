05995c24ceaa5dbfa95fdf01c6e2e27427351449
YunA486 <w2039@e-mirim.hs.kr>
2022-05-22 23:50:02 +0900

    문구

--- turtle_check/JS/turtlecheck.js
+++ turtle_check/JS/turtlecheck.js
@@ -1,74 +1,80 @@
 // More API functions here:
-        // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
+// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
 
-        // the link to your model provided by Teachable Machine export panel
-        const URL = "./my_model/";
-        let model, webcam, ctx, labelContainer, maxPredictions;
+// the link to your model provided by Teachable Machine export panel
+const URL = "./my_model/";
+let model, webcam, ctx, labelContainer, maxPredictions;
 
-        async function init() {
-            const modelURL = URL + "model.json";
-            const metadataURL = URL + "metadata.json";
+async function init() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
 
-            // load the model and metadata
-            // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-            // Note: the pose library adds a tmPose object to your window (window.tmPose)
-            model = await tmPose.load(modelURL, metadataURL);
-            maxPredictions = model.getTotalClasses();
+    // load the model and metadata
+    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+    // Note: the pose library adds a tmPose object to your window (window.tmPose)
+    model = await tmPose.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
 
-            // Convenience function to setup a webcam
-            // 화면 사이즈
-            const size = 600;
-            const flip = true; // whether to flip the webcam
-            webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
-            await webcam.setup(); // request access to the webcam
-            await webcam.play();
-            window.requestAnimationFrame(loop);
+    // Convenience function to setup a webcam
+    // 화면 사이즈
+    const size = 600;
+    const flip = true; // whether to flip the webcam
+    webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
+    await webcam.setup(); // request access to the webcam
+    await webcam.play();
+    window.requestAnimationFrame(loop);
 
-            // append/get elements to the DOM
-            const canvas = document.getElementById("canvas");
-            canvas.width = size;
-            canvas.height = size;
-            ctx = canvas.getContext("2d");
-            labelContainer = document.getElementById("label-container");
-            for (let i = 0; i < maxPredictions; i++) { // and class labels
-                labelContainer.appendChild(document.createElement("div"));
-            }
+    // append/get elements to the DOM
+    const canvas = document.getElementById("canvas");
+    canvas.width = size;
+    canvas.height = size;
+    ctx = canvas.getContext("2d");
+    labelContainer = document.getElementById("label-container");
+    for (let i = 0; i < maxPredictions; i++) { // and class labels
+        labelContainer.appendChild(document.createElement("div"));
+    }
+}
+
+async function loop(timestamp) {
+    webcam.update(); // update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
+}
+
+async function predict() {
+    // Prediction #1: run input through posenet
+    // estimatePose can take in an image, video or canvas html element
+    const {
+        pose,
+        posenetOutput
+    } = await model.estimatePose(webcam.canvas);
+    // Prediction 2: run input through teachable machine classification model
+    const prediction = await model.predict(posenetOutput);
+
+    // 카메라 켜지고 5초 후 글자 생성
+    setTimeout(function () {
+        // 거북목 여부 문구
+        if (prediction[0].className == "거북목" && prediction[0].probability.toFixed(0) == 1) {
+            labelContainer.childNodes[0].innerHTML = "당신은 거북목입니다."
+        } else if (prediction[1].className == "정상목" && prediction[1].probability.toFixed(0) == 1) {
+            labelContainer.childNodes[0].innerHTML = "당신은 정상입니다."
+        } else {
+            labelContainer.childNodes[0].innerHTML = "조금 더 떨어져서 우측을 바라보고 올바르게 서주세요."
         }
+    }, 5000);
 
-        async function loop(timestamp) {
-            webcam.update(); // update the webcam frame
-            await predict();
-            window.requestAnimationFrame(loop);
+    // finally draw the poses
+    drawPose(pose);
+}
+
+function drawPose(pose) {
+    if (webcam.canvas) {
+        ctx.drawImage(webcam.canvas, 0, 0);
+        // draw the keypoints and skeleton
+        if (pose) {
+            const minPartConfidence = 0.5;
+            tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
+            tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
         }
-
-        async function predict() {
-            // Prediction #1: run input through posenet
-            // estimatePose can take in an image, video or canvas html element
-            const {
-                pose,
-                posenetOutput
-            } = await model.estimatePose(webcam.canvas);
-            // Prediction 2: run input through teachable machine classification model
-            const prediction = await model.predict(posenetOutput);
-
-            for (let i = 0; i < maxPredictions; i++) {
-                const classPrediction =
-                    prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-                labelContainer.childNodes[i].innerHTML = classPrediction;
-            }
-
-            // finally draw the poses
-            drawPose(pose);
-        }
-
-        function drawPose(pose) {
-            if (webcam.canvas) {
-                ctx.drawImage(webcam.canvas, 0, 0);
-                // draw the keypoints and skeleton
-                if (pose) {
-                    const minPartConfidence = 0.5;
-                    tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
-                    tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
-                }
-            }
-        }
+    }
+}
